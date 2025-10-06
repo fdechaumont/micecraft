676870855ff99aa6bfa37a6766d125540dd3e6e4
fdechaumont <fabrice.de.chaumont@gmail.Com>
2025-10-06 15:20:48 +0200

    report: add report.setDownloadableContent()

--- src/micecraft/soft/report/Report.py
+++ src/micecraft/soft/report/Report.py
@@ -7,10 +7,9 @@ Created on 5 févr. 2025
 import os
 
 from jinja2 import Environment, FileSystemLoader
-
+from experiments.api.report.ReportTools import clean_filename
 from datetime import datetime
-from micecraft.soft.report.ReportTools import clean_filename
-
+import pandas as pd
 
 '''
 # Create the jinja2 environment.
@@ -27,11 +26,33 @@ class Report(object):
         self.template = template
         self.experimentName = experimentName
         self.style = style # can be: primary, success, danger, warning
+        self._errorLevel = 0 # override the style if different from 0. 1: warning, 2:danger
         self.options = options
+        self.downloadableContent = {} # to add dataframe or files that will be available for download. Key is the name of the link on the webpage. 
     
     def __str__(self, *args, **kwargs):
         return f"Report {self.title} - {self.experimentName}"
+        
+    def setDownloadableContent(self , name , content ):
+        '''
+        content supports dataframe
+        '''
+        self.downloadableContent[name] = content 
     
+    def setErrorLevel( self, errorLevel ):
+        self._errorLevel = errorLevel
+        # overrides default style with errorLevel
+        if self._errorLevel == 1:
+            self.style="success"
+        if self._errorLevel == 1:
+            self.style="warning"
+        if self._errorLevel == 2:
+            self.style="danger"
+                
+    def getErrorLevel( self ):
+        return self._errorLevel
+        
+        
     def render(self , templateFolder, outFolder=None, reportList=None ):
 
         print(f"Rendering {self.experimentName} - {self.title} - {self.template}")
@@ -61,8 +82,25 @@ class Report(object):
             
             return render
         
+        # add extra content
+        extraDownloadContent =""
         
-        render = env.get_template( self.template ).render( title=numberInTitle+self.title, content=self.data, style = self.style, **self.options )
+        for k,v in self.downloadableContent.items():            
+            if isinstance(v, pd.DataFrame):                
+                df = v
+                s = f"{self.experimentName} {self.title} {k}"
+                s = clean_filename( s )
+                fileNameXLS = f"{s}.xlsx"
+                df.to_excel( f"{outFolder}/{fileNameXLS}" )
+                print(f"Xlsx file is : {fileNameXLS}")
+                extraDownloadContent+=f"<a href='{fileNameXLS}' >{k}</a><br>"
+            else:
+                print(f"Report rendering, downloadableContent: Can't process data of type {type(v)}")
+                print( v )
+                quit()
+        
+        
+        render = env.get_template( self.template ).render( title=numberInTitle+self.title, content=self.data, style = self.style, extraDownloadContent=extraDownloadContent, **self.options )
         
         return render
         
